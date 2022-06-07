c29a8b1da5939f76632f1ef3c1e06ad32d8c9562
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2022-06-07 12:53:54 +0900

    ランダムの数値リストから、ビンゴカードを作成できるようにする

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,46 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class Main {
-
     public static void main(String[] args) {
-	System.out.println("test");
+        ArrayList<Integer> numList = new ArrayList<>();
 
-    System.out.println("test");
+        //1~20の整数値を持つリストを用意
+        for (int i = 0; i <= 30; i++) {
+            numList.add(i);
+        }
+
+        //リストを表示（確認用）
+        System.out.println("作成したリストはこちらです");
+        System.out.println(numList);
+
+
+        //shuffleメソッドで上で作ったリストをシャッフル
+        Collections.shuffle(numList);
+
+        int rows = 5;
+        int columns = 5;
+        int counter = 0;
+
+        int[][] bingoCard = new int[rows][columns];
+        //ランダムにした結果を表示
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < columns; j++) {
+                bingoCard [i][j] = numList.get(counter) + 1;
+                counter++;
+            }
+        }
+
+        for (int[] ints : bingoCard) {
+            for (int anInt : ints) {
+                System.out.print(String.format("%02d", anInt) + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
