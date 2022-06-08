a1e7ae3b4624ac68edafecc16363b2c6fb9bb52f
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2022-06-08 13:39:46 +0900

    readmeに要件定義と必要そうなクラスについて追記した

--- src/com/company/BingoCard.java
+++ src/com/company/BingoCard.java
@@ -4,25 +4,36 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
-public class Main {
+public class BingoCard {
+    int rows = 5;
+    int columns = 5;
+    int counter = 0;
+    int[][] cardNumber = new int[rows][columns];
+    ArrayList<Integer> numList = new ArrayList<>();
 
-    public static void main(String[] args) {
-        BingoCard card = new BingoCard();
-
-        System.out.println(card.numList);
-
-//        変数bingoCardに作成したビンゴカードを格納
-//        int[][] bingoCard = makeBingoCard();
-
-        // bingoCardの表示
-        for (int[] ints : card.cardNumber) {
-            for (int anInt : ints) {
-                System.out.print(String.format("%02d", anInt) + " ");
-            }
-            System.out.println();
+    // WIP:列ごとに決まった範囲の数値のリストを作成したい（うまくいっていない）
+    public ArrayList<Integer> makeNumList(int rowNumber){
+        for (int i = 1; i <= 15 * (rowNumber + 1); i++) {
+            numList.add(i);
         }
 
+        return numList;
     }
+
+    // 5 * 5の列のとき、第1列(cardNumber[0~4][0])には1~15の数字が入る。
+    // cardNumber[rows][columns]とした時、15 * (rows + 1)とすれば
+    // その列に入る数字の上限が分かる。
+
+    public BingoCard() {
+        for (int i = 0; i < rows; i++) {
+            makeNumList(i);
+            for (int j = 0; j < columns; j++) {
+                cardNumber[i][j] = numList.get(counter);
+                counter++;
+            }
+        }
+    }
+
 
 //    public static int[][] makeBingoCard() {
 //        ArrayList<Integer> numList = new ArrayList<>();
